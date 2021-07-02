a3075ede60ef0f23a75ea1d815db9edd221f6685
devcarlin <carlos.henrique>
2021-07-02 13:27:04 -0300

    feat: menu #2

---
+++ menu #2/script.js
@@ -0,0 +1,19 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+
+  nav.classList.toggle('active');
+
+  const active = nav.classList.contains('active');
+
+  event.currentTarget.setAttribute('arial-expanded', active);
+
+  if (active) event.currentTarget.setAttribute('arial-label', 'Fechar Menu');
+
+  if (!active) event.currentTarget.setAttribute('arial-label', 'Abrir Menu');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
